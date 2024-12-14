e17b17a6cab4b48ad5cf795ca483ad8cbf237148
Volodymyr Chornonoh <chernonog.vova@gmail.com>
2024-12-14 13:08:18 +0200

    day 14 refactor

--- day-14/src/main.rs
+++ day-14/src/main.rs
@@ -5,27 +5,25 @@ struct Robot {
 }
 
 impl Robot {
-    fn advance(&mut self, rows: i32, cols: i32) {
+    fn x(&self) -> usize {
+        self.position.0 as usize
+    }
+
+    fn y(&self) -> usize {
+        self.position.1 as usize
+    }
+
+    fn advance(&mut self, rows: usize, cols: usize) {
         let next = (
             self.position.0 + self.velocity.0,
             self.position.1 + self.velocity.1,
         );
 
-        self.position.0 = if next.0 >= cols {
-            next.0 - cols
-        } else if next.0 < 0 {
-            cols + next.0
-        } else {
-            next.0
-        };
+        let rows = rows as i32;
+        let cols = cols as i32;
 
-        self.position.1 = if next.1 >= rows {
-            next.1 - rows
-        } else if next.1 < 0 {
-            rows + next.1
-        } else {
-            next.1
-        };
+        self.position.0 = (next.0 % cols + cols) % cols;
+        self.position.1 = (next.1 % rows + rows) % rows;
     }
 }
 
@@ -66,25 +64,90 @@ impl std::str::FromStr for Robot {
     }
 }
 
-fn parse_robots(input: &str) -> Result<Vec<Robot>, String> {
-    input.lines().map(|l| l.parse::<Robot>()).collect()
+struct Matrix {
+    robots: Vec<Robot>,
+    map: Vec<Vec<usize>>,
+    visual_map: Vec<Vec<char>>,
+    rows: usize,
+    cols: usize,
 }
 
-fn advance_robots(robots: &mut Vec<Robot>, rows: i32, cols: i32) {
-    for robot in robots.iter_mut() {
-        robot.advance(rows, cols);
+impl Matrix {
+    fn new(input: &str, rows: usize, cols: usize) -> Result<Self, String> {
+        let robots: Vec<Robot> = input
+            .lines()
+            .map(|l| l.parse::<Robot>())
+            .collect::<Result<_, _>>()?;
+        let map = Matrix::get_map(&robots, rows, cols);
+        let visual_map = Matrix::get_visual_map(&map);
+        Ok(Self {
+            robots,
+            rows,
+            cols,
+            map,
+            visual_map,
+        })
+    }
+
+    fn get_map(robots: &[Robot], rows: usize, cols: usize) -> Vec<Vec<usize>> {
+        let mut map = vec![vec![0; cols]; rows];
+        for robot in robots.iter() {
+            let old = map[robot.y()][robot.x()];
+            map[robot.y()][robot.x()] = old + 1;
+        }
+        map
+    }
+
+    fn get_visual_map(map: &[Vec<usize>]) -> Vec<Vec<char>> {
+        map.iter()
+            .map(|l| l.iter().map(|n| if *n > 0 { '#' } else { ' ' }).collect())
+            .collect()
+    }
+
+    fn advance_robots(&mut self) {
+        for robot in self.robots.iter_mut() {
+            let (old_x, old_y) = (robot.x(), robot.y());
+            robot.advance(self.rows, self.cols);
+            let (new_x, new_y) = (robot.x(), robot.y());
+
+            self.map[old_y][old_x] -= 1;
+            if self.map[old_y][old_x] == 0 {
+                self.visual_map[old_y][old_x] = ' ';
+            }
+            self.map[new_y][new_x] += 1;
+            if self.map[new_y][new_x] > 0 {
+                self.visual_map[new_y][new_x] = '#';
+            }
+        }
+    }
+
+    fn has_easter_egg(&self) -> bool {
+        let mut j = 0;
+
+        for line in &self.visual_map {
+            if j == PATTERN.len() {
+                return true;
+            }
+
+            let line_str: String = line.iter().collect();
+            if line_str.contains(PATTERN[j]) {
+                j += 1;
+            }
+        }
+
+        j == PATTERN.len()
     }
 }
 
-fn get_robot_positions(robots: &[Robot], rows: i32, cols: i32) -> String {
-    let mut matrix: Vec<String> = (0..rows).map(|_| " ".repeat(cols as usize)).collect();
-    for robot in robots {
-        matrix[robot.position.1 as usize].replace_range(
-            (robot.position.0 as usize)..(robot.position.0 as usize + 1),
-            "#",
-        );
+impl std::fmt::Display for Matrix {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let map: Vec<String> = self
+            .visual_map
+            .iter()
+            .map(|line| -> String { line.iter().collect() })
+            .collect();
+        f.write_str(&map.join("\n"))
     }
-    matrix.join("\n")
 }
 
 const PATTERN: [&str; 33] = [
@@ -123,48 +186,29 @@ const PATTERN: [&str; 33] = [
     "###############################",
 ];
 
-fn has_easter_egg(matrix: &str) -> bool {
-    let mut contains = false;
-    let mut j = 0;
-
-    for line in matrix.lines() {
-        if j == PATTERN.len() {
-            return true;
-        }
-
-        contains = if line.contains(PATTERN[j]) {
-            j += 1;
-            true
-        } else {
-            false
-        };
-    }
-
-    contains
-}
+const MAX_ITERATIONS: usize = 20000;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let filename = &args[1];
-    let cols = &args[2].parse::<i32>().expect("Failed to parse cols");
-    let rows = &args[3].parse::<i32>().expect("Failed to parse rows");
+    let cols = &args[2].parse::<usize>().expect("Failed to parse cols");
+    let rows = &args[3].parse::<usize>().expect("Failed to parse rows");
 
     let input = std::fs::read_to_string(filename).expect("Failed to read file");
-    let mut robots = parse_robots(&input).expect("Failed to parse robots");
+    let mut matrix = Matrix::new(&input, *rows, *cols).expect("Failed to parse input");
 
     let mut cnt = 0;
 
     loop {
-        advance_robots(&mut robots, *rows, *cols);
+        matrix.advance_robots();
         cnt += 1;
-        let matrix = get_robot_positions(&robots, *rows, *cols);
-        if has_easter_egg(&matrix) {
+        if matrix.has_easter_egg() {
             println!("{}", matrix);
             println!("cnt: {}", cnt);
             break;
         }
 
-        if cnt > 20000 {
+        if cnt > MAX_ITERATIONS {
             eprintln!("Couldn't find easter egg");
             break;
         }
